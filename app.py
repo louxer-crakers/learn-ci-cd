84a068269946546485a0b7f9289e62df8d235c05
feradi <feriadiputra6@gmail.com>
2026-08-27 19:52:08 +0700

    add ci

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ def home():
 @app.route("/health")
 def health():
     return jsonify({"status": "ok", "service": "flask-api"}), 200
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
