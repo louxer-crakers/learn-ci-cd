6af932124d1163438a00269688ceabe26c664303
feradi <feriadiputra6@gmail.com>
2026-08-27 19:58:55 +0700

    add ci

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "hello from crocodic"
 
+
 @app.route("/health")
 def health():
     return jsonify({"status": "ok", "service": "flask-api"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
